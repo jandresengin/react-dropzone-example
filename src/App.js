426d32afd07c5916ab651329505ec72f8f192061
JandresEngineer <jandresengineer@gmail.com>
2022-10-31 00:29:05 -0400

    dropzone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import DropzoneComponent from './DropzoneComponent';
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import Progressbar from './Progress_bar';
+import React, { useEffect, useState } from 'react';
+let progressInterval = null;
+let uploadFile = false;
 
-function App() {
+function App( ) {
+  const [progresshook, setProgresshook] = useState(0);
+ 
+  useEffect(() => {
+    progressInterval = setInterval(() => {
+      if (uploadFile === true){
+        setProgresshook(prev => prev + 1);
+      }
+      
+    }, 100);
+  }, []);
+ 
+  useEffect(() => {
+    if (progresshook >= 100) {
+      clearInterval(progressInterval);
+      setProgresshook( c => 0);
+      uploadFile = false;
+    }
+  }, [progresshook]);
+
+const handleButton = () => {
+  if (uploadFile === false){
+    uploadFile = true;
+  }else{
+    uploadFile = false;
+    clearInterval(progressInterval);
+    setProgresshook( c => 0);
+  }
+ 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DropzoneComponent />
+      <h3 className="heading">Uploading</h3>
+      <Button type="button" onClick={ (event) => handleButton(event) }>
+        +1
+      </Button>
+      <h2>hi {progresshook} hi</h2>
+      <Progressbar bgcolor="#99ccff" progress={progresshook}  height={30} />
     </div>
+
   );
 }
 
